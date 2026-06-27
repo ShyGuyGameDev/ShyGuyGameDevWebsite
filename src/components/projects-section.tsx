cc777d8a58524cf8952a53ecaefff2ccb4d1ea67
ConsoleEmpty <shyguygamedev@gmail.com>
2026-06-26 23:57:05 -0400

    plane

--- src/components/projects-section.tsx
+++ src/components/projects-section.tsx
@@ -11,7 +11,7 @@ const completedProjects = [
     title: "Malice and Mercy",
     url: "https://emptyconsole.github.io/Malice-and-Mercy/",
     date: "June 2025",
-    tag: "Game Jam",
+    tag: "Games",
     description: (
       <>
         A{" "}
@@ -36,11 +36,11 @@ const completedProjects = [
       </>
     ),
     learnings: [
-      "Building a full game loop",
+      "Building a full game",
       "Implementing custom physics",
       "Level design and choice-based gameplay",
       "Teamwork under time pressure",
-      "Managing deadlines and collaborating on art and music",
+      "Managing deadlines while collaborating on art and music",
     ],
     image: "/malice_and_mercy-Picsart-AiImageEnhancer.png",
     // video: "/Clips/MaliceAndMercy.mp4",
@@ -49,7 +49,7 @@ const completedProjects = [
     title: "Space Looper",
     url: "https://emptyconsole.itch.io/space-looper",
     date: "August 2025",
-    tag: "Game Jam",
+    tag: "Games",
     description: (
       <>
         A{" "}
@@ -66,9 +66,8 @@ const completedProjects = [
     ),
     learnings: [
       "Rapid prototyping",
-      "Designing and balancing custom movement and resource systems",
+      "Trying new solutions",
       "Collaboration under tight deadlines",
-      "Debugging complex interactions",
       "Iterative design and precise gameplay mechanics",
     ],
     image: "/Untitled_presentation_1-Picsart-AiImageEnhancer.png",
@@ -78,7 +77,7 @@ const completedProjects = [
     title: "Bugged Out",
     url: "https://emptyconsole.itch.io/bugged-out",
     date: "September 2025",
-    tag: "Game Jam",
+    tag: "Games",
     description: (
       <>
         Created by Empty Console for the{" "}
@@ -94,7 +93,7 @@ const completedProjects = [
       </>
     ),
     learnings: [
-      "Integrating theme into mechanics",
+      "Building around a theme",
       "Iterative design and refining platformer feel",
       "Playtesting and incorporating user feedback",
       "Improving teamwork coordination",
@@ -106,7 +105,7 @@ const completedProjects = [
     title: "Open Stage",
     url: "https://open-stage.vercel.app/signin",
     date: "October 2025",
-    tag: "App Challenge",
+    tag: "Apps",
     description: (
       <>
         A{" "}
@@ -122,15 +121,147 @@ const completedProjects = [
       </>
     ),
     learnings: [
-      "Full-stack development",
+      "Building a real product",
       "UX design and gathering real user feedback",
-      "Sustainable business modeling",
+      "Addressing a problem",
       "Presenting solutions to judges",
       "Creating socially impactful technology",
     ],
     image: "/openstage.png",
     // video: "/Clips/OpenStage.mp4",
   },
+  {
+   title: "First Tech Challenge",
+   url: "https://www.firstinspires.org/programs/ftc/",
+   date: "February 2025",
+   tag: "Robotics",
+   description: (
+     <>
+	ShyGuy’s first time leading a large team was through his school’s competitive robotics program. On the team he joined, he quickly took a leadership role and learned many useful life skills like how to work on a team with varying engagement levels, how to deal with unexpected failures, and how to efficiently use limited resources.
+     </>
+   ),
+   learnings: [
+     "Leading a large team",
+     "Dealing with unexpected failure",
+     "Efficiently use limited resources",
+     "Competitive robotics",
+   ],
+   image: "/FTCPosts.jpg",
+  },
+  {
+    title: "Dimensional Rifter",
+    url: "https://shyguygamedev.github.io/Dimensional-Rifter/",
+    date: "March 2025",
+    tag: "Games",
+    description: (
+      <>
+        ShyGuy’s technical curiosity started by playing video games, and eventually learning how to build them. His first ever game was Dimensional Rifter, a simple platformer where the user has to get around obstacles by throwing their javelin at nodes capable of altering the environment around them. This game taught ShyGuy how to organize a project, design a product, and incorporate user feedback.
+      </>
+    ),
+    learnings: [
+      "Beginner game",
+      "Organizing the project",
+      "Designing a product",
+      "Incorporating user feedback",
+    ],
+    image: "/Dimensional Rifter.png",
+  },
+  {
+    title: "Computer Vision Robot",
+    //url: "https://shyguygamedev.github.io/Dimensional-Rifter/",
+    date: "April 2026",
+    tag: "Robotics",
+    description: (
+      <>
+        The next frontier of AI is spatial intelligence, which ShyGuy chose to explore by attempting to implement it in his own robot. After 6 months, ShyGuy was able to implement YOLO11n, a computer vision program able to distinguish objects and recognize what they are, into his project to create a robotic “dog” capable of moving towards a specified object whenever it sees it.
+      </>
+    ),
+    learnings: [
+      "Spatial intelligence and computer vision",
+      "Combining old technology with new technology",
+      "Experimenting with new programs",
+      "Combining premade tools with custom creations",
+    ],
+    image: "/Screenshot 2026-06-26 at 4.19.34 PM.png",
+    // image: "/futurecity.png",
+  },
+  {
+    title: "Future City",
+    url: "",
+    date: "February 2026",
+    tag: "Miscellaneous",
+    description: (
+      <>
+        ShyGuy entered the Future City competition with 5 other students, and he came out learning so much more than he originally thought he would. Designing a city, writing an essay about it, and creating a model of it’s layout, taught ShyGuy how to put multiple proven concepts together to verify the possiblity of a futuristic technology, how to lead a team where everyone comes from a different background, and how to present complex topics simply.
+      </>
+    ),
+    learnings: [
+      "Verifying the possiblity of future technologies",
+      "Bringing different fields together",
+      "Presenting complex topics simply",
+      "Designing for the future",
+    ],
+    image: "/futurecity.png",
+  },
+  {
+    title: "Cumberland Elementary",
+    url: "",
+    date: "May 2026",
+    tag: "Teaching",
+    description: (
+      <>
+        After meeting the lead of Cumberland Elementary’s after school robotics program and showing him his Computer Vision Dog, ShyGuy was invited to speak at one of the program’s weekly sessions. At this session, ShyGuy spoke about his robotics journey, how he built this robot, and answered questions from the students about their own projects. ShyGuy has always loved his mentors, and was incredibly thankful to Cumberland Elementary for the opportunity to help others like him.
+      </>
+    ),
+    learnings: [
+      "Speaking at a school",
+      "Answering questions from students",
+      "Helping others learn",
+    ],
+    image: "/HomeTopics/Teaching/A.png",
+  },
+  {
+    title: "Berkeley Model United Nations",
+    url: "",
+    date: "November 2025",
+    tag: "MUN",
+    description: (
+      <>
+        ShyGuy’s first MUN conference was BMUN, where ShyGuy was in a crisis commitee, meaning among other things, he represented a person, not a country. He was Nina Witkofsky, the communications director and acting deputy director of the CDC, in a commitee set in a future where a new virus is terorizing the world. ShyGuy won Outstanding at this conference, which is equivalent to 2nd place in the committee.
+      </>
+    ),
+    learnings: [
+      "Representing the CDC",
+      "Trying new experiences",
+      "Representing an individual",
+    ],
+    image: "/HomeTopics/MUN/download (6).png",
+  },
+  {
+    title: "Nueva Model United Nations Conference",
+    url: "",
+    date: "February 2026",
+    tag: "MUN",
+    description: (
+      <>
+        In NMUNC, ShyGuy represented Poland in the EU committee. The topic for the EU was green energy and green technology. This was ShyGuy’s second conference with the format of general assembly, meaning he knew what to expect. Due to this, he decided to prepare a lot more than he had in the past, which turned out to be a good decision. During committee, he lead discussions, was a key writer of his resolution, and used his influence to make important allies in the committee.
+      </>
+    ),
+    learnings: [
+      "Green energy and green technology",
+      "Thorough preparation",
+      "Leading committee",
+      "Making allies",
+    ],
+    image: "/HomeTopics/MUN/Screenshot 2026-06-11 at 7.57.46 PM.png",
+  },
+ 
+ 
+ 
+  
+  
+ 
+ 
 ]
 
 // Sort newest first so the most recent project is top-left and the
